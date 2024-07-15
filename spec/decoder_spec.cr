795235e8daf6c5544ff5bd40dfdd08dce4ca96c6
Walter Kaunda <14844142+kwalter94@users.noreply.github.com>
2024-07-15 10:27:28 +0200

    Add more test samples to decoder specs

--- spec/decoder_spec.cr
+++ spec/decoder_spec.cr
@@ -13,111 +13,143 @@ module Tchipi8
         end
 
         it "decodes JMPNAS (0x0???)" do
-          Decoder.decode(0x0100).should eq(Opcodes::JMPNAS)
-          Decoder.decode(0x00E1).should eq(Opcodes::JMPNAS)
+          (0x0000..0x0FFF).each do |instruction|
+            next if instruction == Opcodes::CLS.opcode || instruction == Opcodes::RET.opcode
 
-          instruction = Random.new.rand(0x0FFF).to_u16
-          if {Opcodes::CLS, Opcodes::RET}.includes?(instruction)
-            Decoder.decode(instruction).should_not eq(Opcodes::JMPNAS)
-          else
-            Decoder.decode(instruction).should eq(Opcodes::JMPNAS)
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::JMPNAS)
           end
         end
       end
 
       context "0x1FFF range instructions" do
         it "decodes JMP (0x1???)" do
-          instruction = Random.new.rand(0x1000..0x1FFF).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::JMP)
+          (0x1000..0x1FFF).each do |instruction|
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::JMP)
+          end
         end
       end
 
       context "0x2FFF range instructions" do
         it "decodes CALL (0x2???)" do
-          instruction = Random.new.rand(0x2000..0x2FFF).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::CALL)
+          (0x2000..0x2FFF).each do |instruction|
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::CALL)
+          end
         end
       end
 
       context "0x3FFF range instructions" do
         it "decodes SKIPIFEQ (0x3???)" do
-          instruction = Random.new.rand(0x3000..0x3FFF).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::SKIPIFEQ)
+          (0x3000..0x3FFF).each do |instruction|
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::SKIPIFEQ)
+          end
         end
       end
 
       context "0x4FFF range instructions" do
         it "decodes SKIPIFEQ (0x4???)" do
-          instruction = Random.new.rand(0x4000..0x4FFF).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::SKIPIFNEQ)
+          (0x4000..0x4FFF).each do |instruction|
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::SKIPIFNEQ)
+          end
         end
       end
 
       context "0x5FF0 range instructions" do
         it "decodes SKIPIFEQV (0x5??0)" do
-          registers = Random.new.rand(0x00..0xFF)
-          instruction = (0x5000 | registers << 4).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::SKIPIFEQV)
+          (0x00..0xFF).each do |registers|
+            instruction = (0x5000 | registers << 4).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::SKIPIFEQV)
+          end
         end
 
         it "raises InvalidInstruction for instruction 0x5??X where X in 1..F" do
           rng = Random.new
-          x = rng.rand(0x0001..0x000F)
-          registers = rng.rand(0x00..0xFF)
-          instruction = (0x5000 | registers << 4 | x).to_u16
-          expect_raises(InvalidInstruction) { Decoder.decode(instruction) }
+          (0x0001..0x000F).each do |x|
+            (0x00..0xFF).each do |registers|
+              instruction = (0x5000 | registers << 4 | x).to_u16
+              expect_raises(InvalidInstruction) { Decoder.decode(instruction) }
+            end
+          end
         end
       end
 
       context "0x6FFF range instructions" do
         it "decodes SET (0x6???)" do
-          instruction = Random.new.rand(0x6000..0x6FFF).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::SET)
+          (0x6000..0x6FFF).each do |instruction|
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::SET)
+          end
         end
       end
 
       context "0x7FFF range instructions" do
         it "decodes ADD (0x7???)" do
-          instruction = Random.new.rand(0x7000..0x7FFF).to_u16
-          Decoder.decode(instruction).should eq(Opcodes::ADD)
+          (0x7000...0x7FFF).each do |instruction|
+            Decoder.decode(instruction.to_u16).should eq(Opcodes::ADD)
+          end
         end
       end
 
       context "0x8FFF range instructions" do
-        it "decodes COPY (0x8FF0)" do
-          Decoder.decode(0x8FF0).should eq(Opcodes::COPY)
+        it "decodes COPY (0x8??0)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8000 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::COPY)
+          end
         end
 
-        it "decodes OR (0x8FF1)" do
-          Decoder.decode(0x8FF1).should eq(Opcodes::OR)
+        it "decodes OR (0x8??1)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8001 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::OR)
+          end
         end
 
-        it "decodes AND (0x8FF2)" do
-          Decoder.decode(0x8FF2).should eq(Opcodes::AND)
+        it "decodes AND (0x8??2)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8002 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::AND)
+          end
         end
 
-        it "decodes XOR (0x8FF3)" do
-          Decoder.decode(0x8FF3).should eq(Opcodes::XOR)
+        it "decodes XOR (0x8??3)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8003 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::XOR)
+          end
         end
 
-        it "decodes ADDV (0x8FF4)" do
-          Decoder.decode(0x8FF4).should eq(Opcodes::ADDV)
+        it "decodes ADDV (0x8??4)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8004 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::ADDV)
+          end
         end
 
-        it "decodes SUBV (0x8FF5)" do
-          Decoder.decode(0x8FF5).should eq(Opcodes::SUBV)
+        it "decodes SUBV (0x8??5)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8005 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::SUBV)
+          end
         end
 
-        it "decodes RSHIFT (0x8FF6)" do
-          Decoder.decode(0x8FF6).should eq(Opcodes::RSHIFT)
+        it "decodes RSHIFT (0x8??6)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8006 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::RSHIFT)
+          end
         end
 
-        it "decodes RSUB (0x8FF7)" do
-          Decoder.decode(0x8FF7).should eq(Opcodes::RSUB)
+        it "decodes RSUB (0x8??7)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x8007 | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::RSUB)
+          end
         end
 
-        it "decodes LSHIFT (0x8FFE)" do
-          Decoder.decode(0x8FFE).should eq(Opcodes::LSHIFT)
+        it "decodes LSHIFT (0x8??E)" do
+          (0x00..0xFF).each do |registers|
+            instruction = (0x800E | (registers << 4)).to_u16
+            Decoder.decode(instruction).should eq(Opcodes::LSHIFT)
+          end
         end
 
         it "raises InvalidInstruction on invalid instructions" do
@@ -125,13 +157,19 @@ module Tchipi8
 
           (0x8...0xE).each do |x|
             instruction = (0x8000 | registers << 4 | x).to_u16
-            expect_raises(InvalidInstruction) { pp({instruction.to_s(16), Decoder.decode(instruction) }) }
+            expect_raises(InvalidInstruction) { Decoder.decode(instruction) }
           end
 
           expect_raises(InvalidInstruction) do
             instruction = (0x8000 | registers << 4 | 0xF).to_u16
-            pp Decoder.decode(instruction)
+            Decoder.decode(instruction)
           end
+        end
+      end
+
+      context "0x9FFF range instructions" do
+        it "decodes SKIPIFNEQV (0x9??0)" do
+          true.should be_false
         end
       end
     end
